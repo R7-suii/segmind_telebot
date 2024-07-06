159e584ddc3271f9ec3153ae7d311553067c58a3
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-07-06 06:47:27 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-main_token = 'main_bot_token'
+main_token = '7069412088:AAHjHTJgiHaRswN5ejuJh7dCIzoqGcGJlxY'
 
-monitor_token = 'monitor_bot_token'
+monitor_token = '6677707468:AAFfezVN3_8Zrmnb5QIfV1ME0hjgsUon8zY'
 
-segmind_api_key = "Your_api_key"
+segmind_api_key = "0510d743-0d6f-4a43-b0ea-a50d891cd5f3"
 
-chat_id = "your_chat_id" 
+chat_id = "-1002015179601" 
